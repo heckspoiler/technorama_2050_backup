0a6e8649b5c1f80a7a397cbb1ea63e1214c5e621
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-12-13 14:23:58 +0100

    adding scroll animation to navbar

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"y2Af5+YcrmrZ1hjU5Jx/XsjoL9f+nBiNHeKCDsLDfvk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"f98UNzw6a7KCm8ljEdzOY/5qTei6hXe8RqpS2RNVxsQ=\"\n}"
--- app/page.js
+++ app/page.js
@@ -2,11 +2,27 @@
 
 import Home from './home/page';
 import { SvgBackground } from '@/components/home/svgBackground/svgBackground';
-
+import { useScrollStoreNav } from '@/stores/navScrollStore';
 import loadingStore from '@/stores/initialLoading';
+import { useEffect } from 'react';
 
 export default function App({ posts }) {
   const loadMainContent = loadingStore((state) => state.loadMainContent);
+  const { setScrolled } = useScrollStoreNav();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      console.log('isScrolled: ', isScrolled);
+      setScrolled(isScrolled);
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [setScrolled]);
+
   return (
     <>
       {loadMainContent && (
